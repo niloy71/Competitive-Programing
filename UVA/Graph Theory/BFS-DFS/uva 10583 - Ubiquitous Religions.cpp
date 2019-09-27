7a60bc677ec2757b55adeeb4399e6c6e23f20896
niloy71 <46756341+niloy71@users.noreply.github.com>
2019-09-27 18:37:06 +0600

    Create uva 10583 - Ubiquitous Religions.cpp

---
+++ UVA/Graph Theory/BFS-DFS/uva 10583 - Ubiquitous Religions.cpp
@@ -0,0 +1,51 @@
+/*
+Used Technique: DFS
+
+Explanation: Assume every person is a node if a graph. Give bidirectional edge between all the persons who believe in same religion.
+The number of connected component of the graph is the answer. we can find it by using DFS.
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int n, m, i, a, b, cnt, tc = 1;
+vector<int>v[50100];
+bool f[50100];
+
+void dfs(int i)
+{
+    f[i] = 1;
+    for(auto a: v[i]){
+        if(f[a] == 0)
+            dfs(a);
+    }
+}
+int main()
+{
+    while(1){
+    scanf("%d %d", &n, &m);
+        if(!n && !m)
+            return 0;
+
+        for(i = 0; i < m; i++){
+            scanf("%d %d", &a, &b);
+            v[a].push_back(b);
+            v[b].push_back(a);
+        }
+        memset(f, 0, sizeof(f));
+        cnt = 0;
+        for(i = 1; i <= n; i++){
+            if(f[i] == 0){
+                cnt++;
+                dfs(i);
+            }
+        }
+        printf("Case %d: %d\n", tc++, cnt);
+        for(i = 1; i <= n; i++)
+            v[i].clear();
+    }
+}
